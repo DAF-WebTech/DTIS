de6d133233585cb94c18e83f369c21c0e8ccc874
David Porter <david.porter@fastmail.com>
2020-12-21 12:31:29 +1000

    update to current prod
    Not used in Matrix, but should be. See Standard Page "stories" [1467917]

--- js/stories.js
+++ js/stories.js
@@ -2,37 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	var stories = JSON.parse(document.getElementById("storiesJson").textContent);
 
+    var categories = %globals_asset_data_attributes:1491151^json_decode^index:select_options^index:value%;
 
-	var options = [{ text: "All", value: "all" }, { text: "Innovation", value: "Advance Queensland" }, { text: "Tourism", value: "Queensland Tourism Development" }];
-	var creators = options.map(function (c) { return c.value; });;
+	var regions = %globals_asset_data_attributes:1491149^json_decode^index:select_options^index:value%
 	
-	stories.forEach(function (s) {
-		s.updated = new Date(s.updated); // fix date type
-
-		// create a thumbnail from the first picture we find in the content
-		var e = document.createElement("div");
-		e.innerHTML = s.content;
-		s.img = e.querySelector("img").outerHTML;
-	});
-
-
 	var app = new Vue({
 		el: "#storyContent",
 		data: {
 			length: stories.length,
 			pageSize: 6,
 			pageNum: 1,
-			creator: "all",
-			options: options
+			regions: regions,
+			categories: categories,
+			selectedRegion: "all",
+			selectedCategory: "all"
+			
 		},
 		computed: {
 			items: function () {
-				var filtered = [];
-				for (var i = 0; i < stories.length; ++i) {
-					if (this.creator == "all" || stories[i].creator == this.creator)
-						filtered.push(stories[i])
-				}
 
+				var filtered = []
+				for(var i = 0; i < stories.length; ++i) {
+				    var story = stories[i]
+				    if ((this.selectedCategory == "all" || story.categories.includes(this.selectedCategory)) &&
+	    			        (this.selectedRegion == "all" || story.regions.includes(this.selectedRegion))) {
+	    			    filtered.push(story)
+	    			}
+				}
+				
 				this.length = filtered.length;
 				var startIndex = (this.pageNum - 1) * this.pageSize;
 				var sliced = filtered.slice(startIndex, startIndex + this.pageSize);
@@ -58,28 +55,9 @@ document.addEventListener("DOMContentLoaded", function () {
 			},
 
 			filter: function (event) {
-				this.creator = document.getElementById("categorySelect").value; // misnamed in document, should be creatorSelect
+				this.selectedCategory = document.getElementById("categorySelect").value;
+				this.selectedRegion = document.getElementById("regionSelect").value;
 				this.pageNum = 1;
-			},
-			truncate: function (html, length) {
-				length = (typeof length == 'undefined' ? 75 : length);/*default value is 75*/
-				var div = document.createElement("div");
-				div.innerHTML = html;
-				// add a space after each element in case we got adjacent elements, we don't want the text to run into each other
-				div.querySelectorAll("*").forEach(function (e) {
-					e.textContent += " ";
-				});
-				var retVal = div.textContent;
-				retVal.replace(/\s{2}/g, " ");// remove double whitespace
-				if (retVal.length > length) // truncate
-					retVal = retVal.substr(0, retVal.lastIndexOf(" ", length)) + "…";
-				return retVal;
-			},
-			formatDate: function (d) {
-				var a = d.substring(4).split(" ");
-				a[1] = a[1].replace(/^0/, "");
-				a[1] += ",";
-				return a.join(" ");
 			}
 		}
 	});
